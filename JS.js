34413693efc25bc7fbd6440069206f32851383ae
gerald_no <geraldonuba23@gmail.com>
2024-01-05 20:47:02 -0400

    JavaScript

---
+++ JS.js
@@ -0,0 +1,37 @@
+var numero = 13;
+if (numero % 2 === 0) {
+    console.log(numero + 'es par');
+} else {
+    console.log(numero + 'no es par');
+}
+
+    if (num % 5 === 0){
+        console.log(num + 'es multiplo de 5');
+    } else{
+        console.log(num+ 'no es multiplo de 5');
+    }
+
+
+//for incrementar
+    for (let i = 0; i < 5; i++) {
+        console.log (i);
+    }
+    // for decrementar 
+    for (let i = 10; i > 0; i--) {
+        console.log (i);
+    }
+
+
+//while incrementar
+
+    let contador = 0;
+    while (contador < 5){
+        contador++;
+        console.log(contador)
+    }
+//while decremetnar
+    let contador1= 10;
+    while (contador >= 0){
+        contador1--;
+        console.log(contador1)
+    }
